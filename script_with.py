cc691fc3e68a7aed130432ff071c7c44862f8b82
TROUTAL <57239510+TROUTAL@users.noreply.github.com>
2019-11-12 16:24:49 -0700

    Run this Script with Unclipped Data
    This is the most current verison of the master script WITHOUT Matt's DEM portion.  This code clips each input layer by the boundaries, runs specific euclidean and reclassify functions.  It then converts the rasters to tif format and converts them to a numpy array.  The numpy arrays are appended into one list, and are attempted to be added all together (I believe it fails at this).  It then attempts to write this added numpy array to a TIF file using rasterio.  The ouput is a strange raster with values ranging from 1-22.  The correct final raster should only range from 0-to the nunber of inputs given.

---
+++ script_with.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 10:32:55 2019
+
+@author: Ian Heckman, Matt Berns, & Alex Trout
+"""
+# importing libraries 
+import arcpy 
+from arcpy import env
+from arcpy.sa import *
+import os
+import rasterio 
+import numpy as np
+
+# setting workspace 
+env.workspace = r'D:\Programming\Suitability_project\data_with'
+boundary = 'Project_Bounds.shp'
+env.extent = boundary 
+env.mask = boundary 
+env.overwriteOutput = 1 
+
+unclipped_shps = ['Highways.shp', 'Critical_habitat.shp', 'Wetlands.shp']
+clip_results = ['proj_roads.shp', 'proj_habitat.shp', 'proj_wetlands.shp']
+dists = ['roads_dist', 'habitat_dist', 'wetlands_dist']
+buffered = ['roads_buff', 'habitat_buff', 'wetlands_buff']
+
+# clipping each input feature to the project boundaries 
+# appending wetlands clipped file to clipped list as it was already clipped
+for u, c in zip(unclipped_shps, clip_results): 
+    arcpy.Clip_analysis(u, boundary, c)
+
+# running euclidean distance function for each input
+# reclassifying euclidean distance outputs
+counter = 0 
+for r, d, b in zip(clip_results, dists, buffered):
+    if counter == 0:
+        d = arcpy.sa.EucDistance(r, 300000, 30)
+        reclassify = arcpy.sa.Reclassify(d, 'Value', RemapRange([[0, 90, 1], [90, 300000, 0]]))
+        reclassify.save(b)
+        counter = counter + 1
+    elif counter == 1: 
+        d = arcpy.sa.EucDistance(r, 300000, 30)
+        reclassify = arcpy.sa.Reclassify(d, 'Value', RemapRange([[0, 510, 0], [510, 300000, 1]]))
+        reclassify.save(b)
+        counter = counter + 1
+    elif counter == 2: 
+        d = arcpy.sa.EucDistance(r, 300000, 30)
+        reclassify = arcpy.sa.Reclassify(d, 'Value', RemapRange([[0, 120, 0], [120, 300000, 1]]))
+        reclassify.save(b)
+
+# copying the rasters created in the Euclidean Distance function and copying them to a TIFF file
+# converting TIFF file to numpy array and appending them to a list 
+ras_arrays = []
+for GRID in buffered:
+    tif = arcpy.CopyRaster_management(GRID, str(GRID) + '.tif')
+    NumpyArray = arcpy.RasterToNumPyArray(GRID)
+    ras_arrays.append(NumpyArray)
+    
+for i in ras_arrays:
+    suitras = np.sum(ras_arrays[0:]) # Attempting to add all numpy rasters together in ras_arrays list. Don't think it's working.
+
+# Writing the added numpy array to a raster
+ras = rasterio.open(r'D:\Programming\Suitability_project\data_with\habitat_buff.tif')
+meta = ras.meta 
+
+# Writing the added numpy array to a raster 
+with rasterio.open(r'D:\Programming\Suitability_project\data_with\Suit_Corridors.tif', 'w', **meta) as dest:
+    dest.write(suitras.astype('uint8'), indexes=1)
